66a8ce720050c59ce46c82bb7381b86d7b4fe417
Advaith Chakma <advaithchakma@gmail.com>
2023-01-26 17:28:27 +0530

    add data types

---
+++ data_types/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    
+    // let guess: u32 = "42".parse().expect("Not a number!");
+    let grade = 10;
+    println!("{}", grade);
+
+    let guess: u32 = "50".parse().expect("Not a number");
+    println!("{}",guess);
+
+}
